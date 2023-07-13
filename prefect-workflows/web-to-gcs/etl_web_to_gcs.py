6601f52b9316f7664bbe58927478a0a3aaf09b87
axxadvighess <kosymcqueen@gmail.com>
2023-07-13 22:29:17 +0100

    consolidated etl pipeline

--- prefect-workflows/web-to-gcs/etl_web_to_gcs.py
+++ prefect-workflows/web-to-gcs/etl_web_to_gcs.py
@@ -1,9 +1,18 @@
-import pandas as pd
-import math
-import json
-import time
-from datetime import date, timedelta
 from pathlib import Path
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from movies_etl import load_movies_data
+from critics_etl import load_critics_data
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """ The main ETL function"""
+    load_movies_data()
+    load_critics_data()
+
+
+if __name__ == '__main__':
+    etl_web_to_gcs()
+
+
 
