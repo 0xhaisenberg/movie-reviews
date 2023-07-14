2df25264fee93b7d4e69d39a6ee88f3741b1892c
axxadvighess <kosymcqueen@gmail.com>
2023-07-14 23:34:04 +0100

    final form of code

--- prefect-workflows/gcs_to_bq/movies_to_bq.py
+++ prefect-workflows/gcs_to_bq/movies_to_bq.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import time
+from datetime import date, timedelta
 from pathlib import Path
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
@@ -16,7 +18,7 @@ def extract_from_gcs() -> pd.DataFrame:
     # API date filter is start_dt:end_dt and uses the following format "YYYY-MM-DD:YYYY-MM-DD"
     date_filter = f'{past_quarter}:{yesterdays_dt}'
 
-    gcs_path = f'data/reviews/{date_filter}.parquet'
+    gcs_path = f'data/review/{date_filter}.parquet'
     gcs_block = GcsBucket.load('movie-reviews')
     gcs_block.get_directory(from_path=gcs_path, local_path='./')
 
@@ -28,12 +30,12 @@ def extract_from_gcs() -> pd.DataFrame:
 @task()
 def write_bq(df: pd.DataFrame) -> None:
     """Write DataFrame to BigQuery"""
-    gcp_credentials_block = GcpCredentials.load("movie-reviews-credentials"))
+    gcp_credentials_block = GcpCredentials.load("movie-reviews-credentials")
     df.to_gbq(
         destination_table='movie_reviews_all.movie_data',
         project_id='movie-reviews-392119',
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
-        chunksize: int = 100_000
+        chunksize=100_000,
         if_exists='replace'
     )
 
@@ -41,10 +43,18 @@ def write_bq(df: pd.DataFrame) -> None:
 @flow()
 def movie_reviews_to_bq():
      """Main ETL flow to load data into BigQuery"""
-     data = extract_from_gcs()
 
-     write_bq(data)
+     try:
+
+        data = extract_from_gcs()
+        print('File Extraction Successful')
+        write_bq(data)
+        print('File Load To BigQuery Successful')
+     except Exception as e:
+        print(f"{e} \nFile Operation not successful, please check errors")
 
 
+"""
 if __name__ == '__main__':
     movie_reviews_to_bq()
+"""
