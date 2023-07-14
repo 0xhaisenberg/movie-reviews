77ac22e372e63ddc6390c1e56f24d77dbd141b9c
axxadvighess <kosymcqueen@gmail.com>
2023-07-14 23:01:26 +0100

    movie reviews code to bq pending testing

---
+++ prefect-workflows/gcs_to_bq/movies_to_bq.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from pathlib import Path
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+
+
+
+
+@task(retries=3)
+def extract_from_gcs() -> pd.DataFrame:
+    """Download trip data from GCS"""
+
+    yesterdays_dt = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+    past_quarter = (date.today() - timedelta(90)).strftime('%Y-%m-%d')
+    # API date filter is start_dt:end_dt and uses the following format "YYYY-MM-DD:YYYY-MM-DD"
+    date_filter = f'{past_quarter}:{yesterdays_dt}'
+
+    gcs_path = f'data/reviews/{date_filter}.parquet'
+    gcs_block = GcsBucket.load('movie-reviews')
+    gcs_block.get_directory(from_path=gcs_path, local_path='./')
+
+    df = pd.read_parquet(gcs_path)
+
+    return df
+
+
+@task()
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("movie-reviews-credentials"))
+    df.to_gbq(
+        destination_table='movie_reviews_all.movie_data',
+        project_id='movie-reviews-392119',
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize: int = 100_000
+        if_exists='replace'
+    )
+
+
+@flow()
+def movie_reviews_to_bq():
+     """Main ETL flow to load data into BigQuery"""
+     data = extract_from_gcs()
+
+     write_bq(data)
+
+
+if __name__ == '__main__':
+    movie_reviews_to_bq()
