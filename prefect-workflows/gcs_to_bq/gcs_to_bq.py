effa68cba68068471552f1dd59b39779a00b1b22
axxadvighess <kosymcqueen@gmail.com>
2023-07-14 23:34:16 +0100

    final form of code

--- prefect-workflows/gcs_to_bq/gcs_to_bq.py
+++ prefect-workflows/gcs_to_bq/gcs_to_bq.py
@@ -1,11 +1,12 @@
+from prefect import flow, task
 from movies_to_bq import movie_reviews_to_bq
 from critics_to_bq import critics_to_bq
 
 @flow()
 def gcs_to_bq() -> None:
     """ The main ETL function"""
-    load_movies_data()
-    load_critics_data()
+    movie_reviews_to_bq()
+    critics_to_bq()
 
 
 if __name__ == '__main__':
